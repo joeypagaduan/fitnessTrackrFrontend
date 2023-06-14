66e474df1492721dc393725be029d38f85cc9d81
Joey Pagaduan <joey.paga220@gmail.com>
2023-06-14 12:58:09 -0700

    wrote home; added logo

--- src/components/home.js
+++ src/components/home.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Fragment } from 'react';
 import { Link } from 'react-router-dom'
 
-const Home = ({}) => {
-    return (
-        <>  
-            <div>
-                <div><h1>Fitness Track.r</h1></div>
-            </div>
-        </>
-    )
-}
-
+const Home = ({ user}) => (
+    <>
+        <h1>Fitness Track.r</h1>
+        <div id="welcomeUser">
+            <img src="./images/fitness_logo.png" alt="Fitness Track.r Logo" id='logo'/>
+            {user?.username &&  <>
+                <p>Welcome {user.username}!</p>
+                <Link to="/profile">View Your Profile</Link>
+            </>}
+        </div>
+    </>
+)
 export default Home;
