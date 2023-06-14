e471e51e5dbae0aa1c0ae5c9c88aaeedd4a1366e
Joey Pagaduan <joey.paga220@gmail.com>
2023-06-14 12:28:59 -0700

    added callAPI helper function

--- src/api/index.js
+++ src/api/index.js
@@ -321,3 +321,33 @@ export const getAllPublicRoutines = async () => {
     }
   }
   
+
+  //CALL API helper function//
+
+const getURL = (path) => {
+  const url = BASE_URL + path;
+  return url
+}
+
+const getOptions = (method, body, token) => ({
+    method: method ? method.toUpperCase() : "GET",
+    headers: {
+        'Content-Type': 'application/json', 
+        ...(token && {'Authorization': `Bearer ${token}`})
+    },
+    ...( body && { body: JSON.stringify(body) }),
+});
+
+export const callAPI = async({path, method, body, token}) => {
+    try {
+        const result = await fetch(
+            getURL(path),
+            getOptions(method, body, token),
+        );
+        const response = await result.json();
+        if (response.error) throw response.error;
+        return response?.data;
+    } catch(e) {
+        console.error(e);
+    }
+}
