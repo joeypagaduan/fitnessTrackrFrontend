238f7307f066fb31bb3e6fb81067a34ee0433171
trantuyen1411 <trantuyen1411@gmail.com>
2023-06-14 23:43:03 -0700

    register still need to generate token

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as ViewActivities } from "./viewActivities";
 export { default as AddActivity } from "./addActivity";
 export { default as Home } from "./home";
+export { default as Register } from "./register";
+export {default as Login} from "./login";
