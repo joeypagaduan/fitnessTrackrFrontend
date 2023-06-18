e6976f1eb0fefa12e3ff78d20cf7985c323b207f
Damylles Rose <damylleskopper@gmail.com>
2023-06-17 21:58:12 -0700

    fecthing routines from api, redering on screen

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
-export { default as ViewActivities } from "./viewActivities";
-export { default as AddActivity } from "./addActivity";
-export { default as Home } from "./home";
-export { default as Register } from "./register";
-export {default as Login} from "./login";
+export { default as ViewActivities } from './viewActivities';
+export { default as AddActivity } from './addActivity';
+export { default as Home } from './home';
+export { default as Register } from './register';
+export { default as Login } from './login';
+export { default as Routines } from './routines';
